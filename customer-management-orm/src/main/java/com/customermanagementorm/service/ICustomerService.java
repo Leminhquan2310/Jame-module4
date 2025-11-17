a6cc1d661eaaae20930298ee19f34d4e1dd62894
minhquan <quanleminh2310@gmail.com>
2025-11-17 12:35:42 +0700

    Add customer-management-orm project

---
+++ customer-management-orm/src/main/java/com/customermanagementorm/service/ICustomerService.java
@@ -0,0 +1,15 @@
+package com.customermanagementorm.service;
+
+import com.customermanagementorm.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void remove(int id);
+}
