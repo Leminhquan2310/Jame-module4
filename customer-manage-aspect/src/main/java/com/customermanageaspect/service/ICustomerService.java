d916d6b1292c7611abe7b4bc608847b3074d09e2
minhquan <quanleminh2310@gmail.com>
2025-11-30 21:20:10 +0700

    Add customer-manage-aspect project

---
+++ customer-manage-aspect/src/main/java/com/customermanageaspect/service/ICustomerService.java
@@ -0,0 +1,11 @@
+package com.customermanageaspect.service;
+
+import com.customermanageaspect.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll() throws Exception;
+
+    Customer findOne(Long id) throws Exception;
+}
